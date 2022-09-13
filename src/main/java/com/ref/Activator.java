f0b7112e2405625c3c18c366267ef61c859fde48
Diego Pires <diegospf13@gmail.com>
2022-09-13 15:21:41 -0300

    Removing Openmbee classes

--- src/main/java/com/ref/Activator.java
+++ src/main/java/com/ref/Activator.java
@@ -7,13 +7,10 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
-import org.apache.http.client.ClientProtocolException;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
 import com.ref.fdr.FdrWrapper;
-import com.ref.openmbee.Communication;
-import com.ref.openmbee.adapter.Activity;
 
 public class Activator implements BundleActivator {
 
@@ -51,22 +48,6 @@ public class Activator implements BundleActivator {
 		}
 		if (!f.delete())
 			throw new FileNotFoundException("Failed to delete file: " + f);
-	}
-
-	
-	
-	public static void main(String args[]) throws ClientProtocolException, IOException {
-		
-		String url = "http://18.188.75.184/projects/tmt/refs/master/elements/";
-		String login = "ufrpe";
-		String password = "thisisapassword";
-		String idActivity = "_17_0_2_3_41e01aa_1386574999817_391486_76808";//setup aps
-		String idActivity2 = "_17_0_2_3_41e01aa_1382473227299_833303_50967";//retrive reference
-		String idActivity3 = "_18_0_2_baa02e2_1423614941566_531457_145750"; //teste pinos
-		Activity activity = Communication.buildActivity(url,login,password,idActivity);
-		System.out.println(activity);
-		
-		
 	}
 
 
